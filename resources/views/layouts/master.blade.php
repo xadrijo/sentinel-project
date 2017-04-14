6e1a99813c7b9825bcae18e62edc60c0572f8c6a
Jorge Jimenez <george@shocklogic.com>
2017-04-14 01:26:37 -0400

    Menu done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,17 +20,9 @@
 
     <div class="container">
       <div class="masthead">
-        <h3 class="text-muted">Project name</h3>
-        <nav>
-          <ul class="nav nav-justified">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Downloads</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
+        <h3 class="text-muted">Auth-Sentinel</h3>
+        
+        @include('layouts.nav')
       </div>
 
       @yield('content')
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul class="nav nav-justified">
+        <li><a href="/">Home</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+    </ul>
+</nav>
